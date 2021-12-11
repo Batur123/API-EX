9d9d2394fc84212d29f4496a93eb51f27908aa44
Batuhan Özkoç <batur.ozkoc@gmail.com>
2021-12-11 17:45:04 +0300

    Update Database.js

--- database/Database.js
+++ database/Database.js
@@ -1,1 +1,19 @@
+const Database = require('better-sqlite3')(__dirname+'/database/SQLiteDatabase.db');
+exports.DB = Database;
 
+function CheckIfUsersExists()
+{
+   const SelectTable = Database.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name=?").get("users");
+   
+   if(typeof SelectTable != 'undefined')
+   {
+      Database.exec("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, email TEXT, registerip TEXT)");
+   }
+}
+
+function CheckDatabase()
+{
+   CheckIfUsersExists();
+}
+
+exports.CheckDatabase = CheckDatabase;
