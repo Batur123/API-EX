033d39da549f1379cdbc0d86bad36d19008d9a75
Batuhan Özkoç <batur.ozkoc@gmail.com>
2022-06-06 14:41:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,87 +21,76 @@ app.use("/styles", express.static(__dirname + '/styles'));
 app.use(express.urlencoded({extended: true}))
 
 let ActiveSockets = [];
-const Server = app.listen(port, () =>
-{
-    console.log(Color.yellow+"API Service - "+`Listening on port: ${port}`);
-})
+const Server = app.listen(port, () => {
+    console.log(Color.yellow + "API Service - " + `Listening on port: ${port}`);
+});
 const io = socket(Server);
 
 //Middleware
-io.use((socket, next) =>
-{
-    console.log(Color.green+"[Handshake]: "+Color.magenta+" "+socket.handshake.address.substring(2).split(":")[1]+Color.green+" trying to connect to the socket. It's API Key: "+Color.magenta+" "+socket.handshake.headers.authorization);
+io.use((socket, next) => {
+    console.log(Color.green + "[Handshake]: " + Color.magenta + " " + socket.handshake.address.substring(2).split(":")[1] + Color.green + " trying to connect to the socket. It's API Key: " + Color.magenta + " " + socket.handshake.headers.authorization);
 
-    if(typeof socket == 'undefined' || typeof socket.handshake == 'undefined' || typeof socket.handshake.headers == 'undefined' || typeof socket.handshake.headers.authorization == 'undefined')
-    {
+    if (typeof socket == 'undefined' || typeof socket.handshake == 'undefined' || typeof socket.handshake.headers == 'undefined' || typeof socket.handshake.headers.authorization == 'undefined') {
         return next(new Error("Err 624"));
     }
 
-    if(!Boolean(Func.APIKeyDogrula(socket.handshake.headers.authorization)))
-    {
+    if (!Boolean(Func.APIKeyDogrula(socket.handshake.headers.authorization))) {
         return next(new Error("Err 625"));
     }
 
-    ActiveSockets.push({id: socket.id, ip: socket.handshake.address.substring(2).split(":")[1],apikey: socket.handshake.headers.authorization});
+    ActiveSockets.push({
+        id: socket.id,
+        ip: socket.handshake.address.substring(2).split(":")[1],
+        apikey: socket.handshake.headers.authorization
+    });
     return next();
 });
 
-io.on("connection", function (socket)
-{
-     socket.on('disconnect', () =>
-    {
+io.on("connection", function (socket) {
+    socket.on('disconnect', () => {
         ActiveSockets.filter(word => word.id !== socket.id);
         console.log(Color.yellow+'[Disconnection]:'+Color.green+'This ID disconnected from socket: '+Color.magenta+" "+socket.id);
     });
 });
 
-app.get('*', function(req, res)
-{
+app.get('*', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/', (req, res) =>
-{
+app.get('/', (req, res) => {
     //test (req.headers['x-forwarded-for'] || req.socket.remoteAddress).split(':').pop()
     res.sendFile(__dirname + '/index.html');
-})
+});
 
-app.post('/notice', function (req, res)
-{
+app.post('/notice', function (req, res) {
     let API = Func.CheckApiKey(req.body.apikey);
     let UserInput = req.body.apikey;
-    
-    if(Object.keys(req.body).length === 0)
-    {
+
+    if (Object.keys(req.body).length === 0) {
         return res.status(400).send({errcode: 313, msg: "Err1"});
     }
 
-    if(typeof UserInput == 'undefined' || !Boolean(UserInput))
-    {
+    if (typeof UserInput == 'undefined' || !Boolean(UserInput)) {
         return res.status(400).send({errcode: 314, msg: "Err2"});
     }
 
-    if(UserInput.length !== API.length)
-    {
+    if (UserInput.length !== API.length) {
         return res.status(400).send({errcode: 314, msg: "Err3"});
     }
 
-    if (!Keygen.timingSafeEqual(Buffer.from(UserInput), Buffer.from(API)))
-    {
+    if (!Keygen.timingSafeEqual(Buffer.from(UserInput), Buffer.from(API))) {
         return res.status(400).send({errcode: 314, msg: "Err4"});
     }
 
-    if(!Func.CheckIfEmpty(req.body.IPAdresi))
-    {
+    if (!Func.CheckIfEmpty(req.body.IPAdresi)) {
         return res.status(400).send({errcode: 315, msg: "Err5"});
     }
 
-    if(!Func.IpAdresKontrol(req.body.IPAdresi))
-    {
+    if (!Func.IpAdresKontrol(req.body.IPAdresi)) {
         return res.status(400).send({errcode: 312, msg: "Err6"});
     }
-    
+
     //snip
 
- 
+
 });
